ad3cee491cc63f0a1791d14da4a74a38d611c660
李延 <liyan@wondersoft.cn>
2022-07-27 15:22:27 +0800

    完成基础的中断

---
+++ includes/kernel/interrup.h
@@ -0,0 +1,11 @@
+//
+// Created by Administrator on 2022/7/27.
+//
+
+#ifndef LIYUX_OS_INTERRUP_H
+#define LIYUX_OS_INTERRUP_H
+
+
+#define FAULT_DIVISION_ERROR            0
+
+#endif //LIYUX_OS_INTERRUP_H
