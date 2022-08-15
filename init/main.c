d7474b4bfbaae6ba31b17fe3e8d43be1abf1371d
李延 <liyan@wondersoft.cn>
2022-08-15 16:23:22 +0800

    向64位中传参

--- init/main.c
+++ init/main.c
@@ -1,7 +1,9 @@
+#include "shared/kmem.h"
+
 //
 // Created by Administrator on 2022/8/15.
 //
-int main(){
+int main(kernel_hold_mem *hold_mem){
 
-    return 1;
+    return hold_mem->stack.start_addr;
 }
